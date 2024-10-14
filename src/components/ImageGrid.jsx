a02668c2d37f36493c33050e24aa2595e7209352
Amir Arif <amirarif619@gmail.com>
2024-10-15 02:08:36 +0800

    added create

--- src/components/ImageGrid.jsx
+++ src/components/ImageGrid.jsx
@@ -1,19 +1,17 @@
-import { useContext } from 'react'
+import { useSelector } from 'react-redux'
 import { Row, Col, Image } from 'react-bootstrap'
-import { ProfileContext } from '../App'
-
 
 
 export default function ImageGrid() {
-    const images = useContext(ProfileContext).posts.map((post) => post.image);
+    const posts = useSelector((state) => state.posts);
 
     const renderImages = () => {
-        return images.map((imageUrl, index) => (
-        <Col md={4} key={index} className="mb-4">
-            <Image src={imageUrl} fluid />
-        </Col>
+        return posts.map((post) => (
+            <Col md={4} key={post.id} className="mb-4">
+                <Image src={post.image} fluid />
+            </Col>
         ))
-}
+    }
 
 return <Row>{renderImages()}</Row>
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+
+import ReactDOM from "react-dom/client"
 import App from './App.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { Provider } from "react-redux";
+import store from "./store.js"
+import React from "react";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
     <App />
-  </StrictMode>,
+    </Provider>
+  </React.StrictMode>,
 )
